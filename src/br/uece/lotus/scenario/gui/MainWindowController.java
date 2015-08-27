1c9933674edf8e225127d2753881e7b013b850f4
Laryssa Muniz <lary.llm@gmail.com>
2015-08-26 22:38:08 -0300

    Added functionality to export scenarios for the xml that can be viewed with the LoTuS tool

--- src/br/uece/lotus/scenario/gui/MainWindowController.java
+++ src/br/uece/lotus/scenario/gui/MainWindowController.java
@@ -8,21 +8,29 @@ import br.uece.lotus.scenario.generator.PathGenAllOneLoop;
 import br.uece.lotus.scenario.struct.LtsInfo;
 import br.uece.lotus.scenario.struct.PathSet;
 import br.uece.lotus.viewer.ComponentViewImpl;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javax.swing.JOptionPane;
 
 public class MainWindowController implements Initializable {
 
@@ -30,6 +38,9 @@ public class MainWindowController implements Initializable {
     private ScrollPane mScrollPane;
 
     protected ComponentViewImpl mViewer;
+
+    @FXML
+    protected Button mButtonExport;
 
     @FXML
     private TableView<MainWindowController.PathView> mTableView;
@@ -41,14 +52,14 @@ public class MainWindowController implements Initializable {
     private TableColumn<MainWindowController.PathView, String> mCollumnList;
 
     @FXML
-    private TableColumn<MainWindowController.PathView, Double> mCollumnProbability;
+    private TableColumn<MainWindowController.PathView, String> mCollumnProbability;
 
     protected ObservableList<MainWindowController.PathView> mScenarios = FXCollections.observableArrayList();
     
     LtsInfo mLtsInfo;
     PathGenAllOneLoop mPathGenerator = new PathGenAllOneLoop();
     Component mComponent;
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -65,6 +76,34 @@ public class MainWindowController implements Initializable {
         
         PathSet pathSet = mPathGenerator.generate(mLtsInfo, TestBundleBuilder.build(mLtsInfo, null));     
         fillTable(pathSet);
+        
+        mButtonExport.setOnAction((ActionEvent event) -> {
+            
+            if (mTableView.getSelectionModel().getSelectedItems().isEmpty()){
+                return;
+            }
+            
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Scenarios files");
+            fileChooser.setInitialDirectory(
+                    new File(System.getProperty("user.home"))
+            );
+            
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Scenarios XML (*.xml)", "*.xml")
+            );
+            
+            File file = fileChooser.showSaveDialog(null);
+            
+            if (file != null){
+                try{
+                    exportToXML(file);
+                    JOptionPane.showMessageDialog(null, "File Successfully Saved");
+                } catch (IOException e){
+                    JOptionPane.showMessageDialog(null, "File was not Saved succesfully");
+                }
+            }
+        });
     }
     
     private void initEssential() {
@@ -75,8 +114,12 @@ public class MainWindowController implements Initializable {
         
         mScenarios.clear();
         
-        for (List<Transition > transitionList : pathSet.getPathList()) {
-            mScenarios.add(new PathView(transitionList));
+        Integer count = 0;
+        
+        for (List<Transition> transitionList : pathSet.getPathList()) {
+            String nameScenario = "Scenario" + count;
+            mScenarios.add(new PathView(transitionList, nameScenario));
+            count++;
         }
     }
 
@@ -84,17 +127,13 @@ public class MainWindowController implements Initializable {
         
         mCollumnName.setCellValueFactory(new PropertyValueFactory<>("pathLabels"));
         mCollumnList.setCellValueFactory(new PropertyValueFactory<>("pathStates"));
-        mCollumnProbability.setCellValueFactory(new PropertyValueFactory<>("pathProbability"));
-        mCollumnProbability.setOnEditCommit(
-        new EventHandler<CellEditEvent<MainWindowController.PathView, Double>>() {
-            @Override
-            public void handle(CellEditEvent<MainWindowController.PathView, Double> t) {
-                ((PathView) t.getTableView().getItems().get(
-                    t.getTablePosition().getRow())
-                    ).setPathProbability(t.getNewValue());
-            }
-        }
-    );
+        mCollumnProbability.setCellValueFactory(new PropertyValueFactory<>("pathProbability")); 
+
+        mCollumnProbability.setCellFactory(TextFieldTableCell.forTableColumn()); 
+        
+        mCollumnProbability.setOnEditCommit((CellEditEvent<MainWindowController.PathView, String> t) -> {
+            ((PathView) t.getTableView().getItems().get(t.getTablePosition().getRow())).setPathProbability(t.getNewValue());
+        });
         
         mTableView.setItems(mScenarios);
         mTableView.setOnMouseClicked(onTableClick);
@@ -103,42 +142,42 @@ public class MainWindowController implements Initializable {
 
     public class PathView {
         
-        PathView(List<Transition> path) {
+        PathView(List<Transition> path, String nameScenario) {
             
             mPathLabels = "";
             mPathActions = 0;
             mPathStates = "";
             mPathGuards = "";
-            mPathProbability = 1.0;
-            boolean initialTransition = true;
+            mPathProbability = "";
+        
             mTransitionList = path;
+            mStatesList = new ArrayList<>();
             
-            int count = 0;
-            
-            for (Transition t : path) {
+            boolean initialTransition = true;
+                        
+            for (Transition transition : path) {
                 
-                mPathLabels = "Scenario " + count ;
-                mPathStates += t.getDestiny().getLabel() + ", ";
+                if(!mStatesList.contains(transition.getDestiny())){
+                    mStatesList.add(transition.getDestiny());
+                }
+                
+                if(!mStatesList.contains(transition.getSource())){
+                    mStatesList.add(transition.getSource());
+                }
+                
+                mPathLabels = nameScenario;
+                mPathStates += transition.getDestiny().getLabel() + ", ";
 
                 if (initialTransition) {
-                    mPathStates = t.getSource().getLabel() + ", " + mPathStates;
+                    mPathStates = transition.getSource().getLabel() + ", " + mPathStates;
                 }
 
-                if (t.getGuard() != null) {
-                    mPathGuards += t.getGuard() + ", ";
+                if (transition.getGuard() != null) {
+                    mPathGuards += transition.getGuard() + ", ";
                 }
 
                 initialTransition = false;
-
-                double p = 0.0;
-
-                if (t.getProbability() != null){
-                    p = t.getProbability();
-                }
-
-                mPathProbability *= p;
                 
-                count++;
             }
 
             if (path.size() > 0) {
@@ -149,17 +188,17 @@ public class MainWindowController implements Initializable {
                     mPathGuards = mPathGuards.substring(0, mPathGuards.length() - 2);
                 }
 
-                
                 mPathActions = path.size();
             }
         }
 
         private String mPathLabels;
-        private Integer mPathActions;
         private String mPathStates;
         private String mPathGuards;
-        private Double mPathProbability;
+        private Integer mPathActions;
+        private String mPathProbability;
 
+        public List<State> mStatesList;
         public List<Transition> mTransitionList;
 
         public String getPathLabels() {
@@ -170,7 +209,7 @@ public class MainWindowController implements Initializable {
             return mPathActions;
         }
 
-        public Double getPathProbability() {
+        public String getPathProbability() {
             return mPathProbability;
         }
 
@@ -182,111 +221,176 @@ public class MainWindowController implements Initializable {
             return mPathGuards;
         }
 
-        public void setPathProbability(Double mPathProbability) {
+        public void setPathProbability(String mPathProbability) {
             this.mPathProbability = mPathProbability;
         }
+    }
+    
+    protected void exportToXML(File file) throws IOException{
         
-        
-        
+        try (PrintStream out = new PrintStream(file)){
+            
+            String content = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<project version=\"1.0\" name=\""
+                    + mComponent.getName() + "\">\n";
+            
+            for(PathView path : mScenarios){
+                
+                content += "\t<component name=\""+ path.getPathLabels() +"\">\n"+
+                        "\t\t<states>\n";
+                
+                Integer initial = 0;
+                
+                for(State state : path.mStatesList){
+                    
+                    content += "\t\t\t<state id=\""+state.getID()+"\" x=\""+state.getLayoutX()+"\" y=\""
+                            +state.getLayoutY()+"\" label=\""+state.getLabel()+"\"";
+                    
+                    if (initial++ == 0) {
+                        content += " initial=\"true\"";    
+                    }
+                    
+		    content += ">\n\t\t\t</state>\n";
+                }
+                
+                content += "\t\t</states>\n";
+                content += "\t\t<transitions>\n";
+                
+                initial = 0;
+                
+                for(Transition transition : path.mTransitionList){
+                
+                    content += "\t\t\t<transition from=\""+transition.getSource().getID() +"\" to=\""
+                            +transition.getDestiny().getID() +"\" ";
+                            
+                        if(initial++ == 0){
+                            
+                            String str = path.getPathProbability();
+                            
+                            if (str.isEmpty()) {
+                                str = "0";
+                            }
+                            
+                            content += "prob=\""+ str +"\"";
+                        }else{
+                            content += "prob=\"1\"";
+                        }
+                                    
+                    content += " label=\""+transition.getLabel()+"\" view-type=\"0\">";
+                    content += "\t\t\t</transition>\n";
+                }
+               
+                content += "\t\t</transitions>\n";
+                content += "</component>\n";
+            }
+            
+            content += "</project>\n";
+            
+            out.print(content);
+        }
     }
     
     protected void showChoices(State state) {
         
         applyEnableStyle(state);
         
-        for (Transition t : state.getOutgoingTransitions()) {
-            applyChoiceStyle(t);
-            applyChoiceStyle(t.getDestiny());
+        for (Transition transition : state.getOutgoingTransitions()) {
+            applyChoiceStyle(transition);
+            applyChoiceStyle(transition.getDestiny());
         }
     }
 
-    protected void applyEnableStyle(State s) {
-        s.setColor(null);
-        s.setTextColor("black");
-        s.setTextSyle(State.TEXTSTYLE_NORMAL);
-        s.setBorderColor("black");
-        s.setBorderWidth(1);
+    protected void applyEnableStyle(State state) {
+        state.setColor(null);
+        state.setTextColor("black");
+        state.setTextSyle(State.TEXTSTYLE_NORMAL);
+        state.setBorderColor("black");
+        state.setBorderWidth(1);
     }
 
-    protected void applyEnableStyle(Transition t) {
-        t.setColor("black");
-        t.setTextSyle(Transition.TEXTSTYLE_NORMAL);
-        t.setTextColor("black");
-        t.setWidth(1);
+    protected void applyEnableStyle(Transition transition) {
+        transition.setColor("black");
+        transition.setTextSyle(Transition.TEXTSTYLE_NORMAL);
+        transition.setTextColor("black");
+        transition.setWidth(1);
     }
 
     protected void applyDisableAll() {
         
-        State s = mComponent.getInitialState();
+        State state = mComponent.getInitialState();
+        
         ArrayList<State> stateList = new ArrayList<>();
         ArrayList<Transition> visitedTransitions = new ArrayList<>();
-        int i = 0;
-        stateList.add(s);
+        
+        int count = 0;
+        
+        stateList.add(state);
 
-        while (i < stateList.size()) {
-            s = stateList.get(i);
-            applyDisabledStyle(s);
-            for (Transition t : s.getOutgoingTransitions()) {
-                if (!stateList.contains(t.getDestiny())) {
-                    stateList.add(t.getDestiny());
+        while (count < stateList.size()) {
+            
+            state = stateList.get(count);
+            applyDisabledStyle(state);
+            
+            for (Transition transition : state.getOutgoingTransitions()) {
+                
+                if (!stateList.contains(transition.getDestiny())) {
+                    stateList.add(transition.getDestiny());
                 }
-                if (!visitedTransitions.contains(t)) {
-                    applyDisabledStyle(t);
-                    visitedTransitions.add(t);
+                
+                if (!visitedTransitions.contains(transition)) {
+                    applyDisabledStyle(transition);
+                    visitedTransitions.add(transition);
                 }
             }
-            ++i;
+            
+            ++count;
         }
     }
 
-    protected void applyDisabledStyle(State s) {
-        s.setColor("#d0d0d0");
-        s.setTextColor("#c0c0c0");
-        s.setTextSyle(State.TEXTSTYLE_NORMAL);
-        s.setBorderColor("gray");
-        s.setBorderWidth(1);
+    protected void applyDisabledStyle(State state) {
+        state.setColor("#d0d0d0");
+        state.setTextColor("#c0c0c0");
+        state.setTextSyle(State.TEXTSTYLE_NORMAL);
+        state.setBorderColor("gray");
+        state.setBorderWidth(1);
     }
 
-    protected void applyDisabledStyle(Transition t) {
-        t.setColor("#d0d0d0");
-        t.setTextColor("#c0c0c0");
-        t.setTextSyle(Transition.TEXTSTYLE_NORMAL);
-        t.setWidth(1);
+    protected void applyDisabledStyle(Transition transition) {
+        transition.setColor("#d0d0d0");
+        transition.setTextColor("#c0c0c0");
+        transition.setTextSyle(Transition.TEXTSTYLE_NORMAL);
+        transition.setWidth(1);
     }
 
-    private void applyChoiceStyle(Transition t) {
-        t.setColor("blue");
-        t.setTextSyle(Transition.TEXTSTYLE_BOLD);
-        t.setTextColor("blue");
-        t.setWidth(2);
+    private void applyChoiceStyle(Transition transition) {
+        transition.setColor("blue");
+        transition.setTextSyle(Transition.TEXTSTYLE_BOLD);
+        transition.setTextColor("blue");
+        transition.setWidth(2);
     }
 
-    private void applyChoiceStyle(State s) {
-        s.setColor(null);
-        s.setBorderColor("blue");
-        s.setTextSyle(Transition.TEXTSTYLE_BOLD);
-        s.setTextColor("blue");
-        s.setBorderWidth(2);
+    private void applyChoiceStyle(State state) {
+        state.setColor(null);
+        state.setBorderColor("blue");
+        state.setTextSyle(Transition.TEXTSTYLE_BOLD);
+        state.setTextColor("blue");
+        state.setBorderWidth(2);
     }
 
-    protected EventHandler<? super MouseEvent> onTableClick = new EventHandler<MouseEvent>() {
-
-        @Override
-        public void handle(MouseEvent e) {
-            PathView selectedPath = mTableView.getSelectionModel().getSelectedItem();
-
-            if (selectedPath == null) {
-                System.out.println("PATH VIEW NULL");
-                return;
-            }
-
-            applyDisableAll();
-
-            for (Transition t : selectedPath.mTransitionList) {
-                applyEnableStyle(t);
-                applyEnableStyle(t.getDestiny());
-                applyEnableStyle(t.getSource());
-            }
+    protected EventHandler<? super MouseEvent> onTableClick = (MouseEvent e) -> {
+        
+        PathView selectedPath = mTableView.getSelectionModel().getSelectedItem();
+        
+        if (selectedPath == null) {
+            System.out.println("PATH VIEW NULL");
+            return;
+        }
+        
+        applyDisableAll();
+        
+        for (Transition transition : selectedPath.mTransitionList) {
+            applyEnableStyle(transition);
+            applyEnableStyle(transition.getDestiny());
+            applyEnableStyle(transition.getSource());
         }
     };
 }
